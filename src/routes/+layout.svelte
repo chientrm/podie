2aa13f47822e9f89dad0832696d569bf6ede72c2
chientrm <chientrm@gmail.com>
2022-08-27 16:28:48 +0000

    fix: remove github scope

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,7 +12,7 @@
 	import type { LayoutServerData } from './$types';
 
 	export let data: LayoutServerData;
-	const scope = ['read:user', 'repo:status'].join(' ');
+	const scope = ['repo:status'].join(' ');
 
 	const gsiteVerification = 'gG8WXVPtqVVAJlnJb5v0LlC0-HBSCVSWsVqa7KHwTPA';
 </script>
