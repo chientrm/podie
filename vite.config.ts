0df0d3944fb8300191e98d789f90e02dbe1fc48b
chientrm <chientrm@gmail.com>
2022-08-27 16:01:29 +0000

    fix: og image

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: { assetsInlineLimit: 0 }
 };
 
 export default config;
