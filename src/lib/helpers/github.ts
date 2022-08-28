8045d8f1a8345d2ee52c8555ea50d9c7f6da125e
chientrm <chientrm@gmail.com>
2022-08-28 04:25:42 +0000

    refactor

--- src/lib/helpers/github.ts
+++ src/lib/helpers/github.ts
@@ -2,10 +2,18 @@ import { client_id, client_secret } from '$lib/configs/github.json';
 import routes from '$lib/constants/routes';
 import strings from '$lib/constants/strings';
 
+const check_ok = (res: Response) => {
+	if (!res.ok) {
+		throw new Error(JSON.stringify(res));
+	}
+	return res;
+};
+
 export const get_access_token = (code: string) =>
 	fetch(routes.GITHUB.ACCESS_TOKEN({ client_id, client_secret, code }), {
 		headers: { Accept: 'application/json' }
 	})
+		.then(check_ok)
 		.then((res) => res.json<{ access_token: string }>())
 		.then((data) => data.access_token);
 
@@ -17,13 +25,6 @@ export const get_user = (access_token: string) =>
 			'User-Agent': strings.PODIE
 		}
 	})
-		.then((res) => {
-			if (!res.ok) {
-				throw new Error(JSON.stringify(res));
-			}
-			return res.json<any>();
-		})
-		.then((data) => {
-			const user = { ...data, username: data.login };
-			return user;
-		});
+		.then(check_ok)
+		.then((res) => res.json<any>())
+		.then((data) => ({ ...data, username: data.login }));
