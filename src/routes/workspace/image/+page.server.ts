65dd3f502d4574a3a1c3cbefe5f70f080da507ec
chientrm <chientrm@gmail.com>
2022-09-09 00:18:49 +0000

    fix: https://sentry.io/share/issue/99f2f1fec1ff4058b33c9f0864697f8e/

--- src/routes/workspace/image/+page.server.ts
+++ src/routes/workspace/image/+page.server.ts
@@ -2,17 +2,24 @@ import podie from '$lib/constants/podie';
 import { list_images } from '$lib/helpers/gcp';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = ({ locals }) =>
-	list_images({ project: podie.USER.GH(locals.user!.gh!.id).GCP.PID })
-		.then((images) =>
-			(images.items ?? []).map(
-				({ name, status, diskSizeGb, archiveSizeBytes, storageLocations }) => ({
-					name,
-					status,
-					diskSizeGb,
-					archiveSizeGb: (archiveSizeBytes / 2 ** 30).toFixed(2),
-					region: storageLocations ? storageLocations[0] : ''
-				})
-			)
-		)
-		.then((images) => ({ images }));
+export const load: PageServerLoad = async ({ locals }) => {
+	const ghid = locals.user!.gh!.id,
+		project = podie.USER.GH(ghid).GCP.PID,
+		list_imgs = async () => {
+			try {
+				return await list_images({ project });
+			} catch {}
+			return undefined;
+		};
+	const result = await list_imgs(),
+		images = (result?.items ?? []).map(
+			({ name, status, diskSizeGb, archiveSizeBytes, storageLocations }) => ({
+				name,
+				status,
+				diskSizeGb,
+				archiveSizeGb: (archiveSizeBytes / 2 ** 30).toFixed(2),
+				region: storageLocations ? storageLocations[0] : ''
+			})
+		);
+	return { images };
+};
